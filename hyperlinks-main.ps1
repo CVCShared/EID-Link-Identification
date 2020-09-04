9372d7ebf0fd1aea5301de9fa5642586e9c2cc91
CVCShared <68439256+CVCShared@users.noreply.github.com>
2020-09-04 11:42:50 -0400

    Add files via upload

---
+++ hyperlinks-main.ps1
@@ -0,0 +1,9 @@
+using module "C:\Users\Nash Ferguson\Downloads\Communary.FileExtensions-master\Communary.FileExtensions-master\Communary.FileExtensions.psm1"
+using module "C:\Users\Nash Ferguson\Desktop\El Dorado\docx-search.psm1"
+using module "C:\Users\Nash Ferguson\Desktop\El Dorado\xlsx-search.psm1"
+$Date = Get-Date -Format s
+$CsvName = $Date.tostring() -replace ":","-"
+$Dir = "C:\Users\Nash Ferguson\Desktop\Target Dir"
+
+IdentifyDocxLinks($Dir)
+IdentifyXlsxLinks($Dir)
