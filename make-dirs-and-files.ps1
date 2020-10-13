d15a89f8abf2531ede94fe4b2f4ec3a537f2c003
Nashua Ferguson <nashferguson1@gmail.com>
2020-10-13 11:25:01 -0400

    Added make-dirs-and-files to simplify making dirs and file duplicates

--- make-dirs-and-files.ps1
+++ make-dirs-and-files.ps1
@@ -1,1 +1,25 @@
 # Create sample directories and files to find links in 
+param(
+$MakeFiles,
+$MakeDir,
+$File,
+$WriteDest,
+$DirName,
+$NumDuplicates,
+$Int = 0
+)
+
+if ($MakeFiles){
+    $FileName = (Split-Path $File -Leaf)
+
+while ($Int -lt $NumDuplicates){
+    $WriteName = "$WriteDest\$Int" + "-" + $FileName
+    write-host("Dest is $WriteName")
+    Copy-Item -Path $File -Destination "$WriteName"
+    $Int += 1
+}
+}
+
+if ($MakeDir){
+    New-Item -ItemType Directory -Path $DirName
+}
